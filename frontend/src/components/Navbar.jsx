d98bc22d2d398ead577c5e18263e79fb5d7f1213
Владислав <vladislav@MacBook-Air-Vladislav.local>
2025-07-16 22:41:25 +0300

    remake 2.2

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     const { userId } = useAuthStore();
-
+    console.log(userId)
     return (
         <nav className="bg-white shadow p-4 flex justify-between items-center">
             <Link to="/" className="font-bold text-xl text-green-700">AgriTradeHub</Link>
             <div className="flex gap-4 items-center">
-                console.log(userId)
+
                 <Link to="/" className="text-gray-700 hover:text-green-700">Объявления</Link>
                 {userId && (
                     <Link
