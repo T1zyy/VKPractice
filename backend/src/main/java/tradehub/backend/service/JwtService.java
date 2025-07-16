d486145c83f7a14b0fca630d06b32db789e8b132
Владислав <vladislav@MacBook-Air-Vladislav.local>
2025-07-17 00:29:58 +0300

    add logs

--- backend/src/main/java/tradehub/backend/service/JwtService.java
+++ backend/src/main/java/tradehub/backend/service/JwtService.java
@@ -4,17 +4,20 @@ import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
 
+@Slf4j
 @Component
 public class JwtService {
 
-    private final String SECRET_KEY = "very-super-secret-key-123456789000000"; // хранить в .env или config
+    private final String SECRET_KEY = "very-super-secret-key-123456789000000";
 
     public String generateToken(String userId) {
+        log.info("Generating token for userId: {}", userId);
         return Jwts.builder()
                 .setSubject(userId)
                 .setIssuedAt(new Date())
@@ -25,13 +28,17 @@ public class JwtService {
 
     public String extractUserId(String token) {
         try {
-            return Jwts.parserBuilder()
+            log.info("Parsing token: {}", token);
+            String userId = Jwts.parserBuilder()
                     .setSigningKey(getKey())
                     .build()
                     .parseClaimsJws(token)
                     .getBody()
                     .getSubject();
+            log.info("Extracted userId: {}", userId);
+            return userId;
         } catch (JwtException e) {
+            log.error("Invalid JWT: {}", e.getMessage());
             return null;
         }
     }
