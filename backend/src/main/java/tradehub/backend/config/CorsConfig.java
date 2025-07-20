b799a08e65addf8a72a72572a1d3b6c74dc71307
Владислав <vladislav@MacBook-Air-Vladislav.local>
2025-07-20 23:27:39 +0300

    fix cors 2.0

--- backend/src/main/java/tradehub/backend/config/CorsConfig.java
+++ backend/src/main/java/tradehub/backend/config/CorsConfig.java
@@ -10,13 +10,17 @@ import java.util.List;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
+
         config.setAllowCredentials(true);
         config.setAllowedOrigins(List.of("https://agri-trade-hub.netlify.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        config.setExposedHeaders(List.of("Authorization"));
+        config.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
