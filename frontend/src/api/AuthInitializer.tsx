7c6642399d3b46e7f535591a4639e7804b21091c
Владислав <vladislav@MacBook-Air-Vladislav.local>
2025-07-24 13:06:33 +0300

    fixex 2/1

--- frontend/src/api/AuthInitializer.tsx
+++ frontend/src/api/AuthInitializer.tsx
@@ -1,12 +1,15 @@
 import { useEffect } from 'react';
+import bridge from '@vkontakte/vk-bridge';
 import { useNavigate } from 'react-router-dom';
 import { useAuthStore } from '../store/authStore';
 
-export default function AuthInitializer() {
+const AuthInitializer = () => {
     const { setAuth } = useAuthStore();
     const navigate = useNavigate();
 
     useEffect(() => {
+        bridge.send('VKWebAppInit');
+
         const accessToken = localStorage.getItem('accessToken');
         const refreshToken = localStorage.getItem('refreshToken');
         const userId = localStorage.getItem('userId');
@@ -19,4 +22,6 @@ export default function AuthInitializer() {
     }, []);
 
     return null;
-}
+};
+
+export default AuthInitializer;
